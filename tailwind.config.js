61f143339864eca787b93a8c78ee020de5e4e705
MailingDelgadoMedina <mailing.delgado.scitexas@gmail.com>
2023-04-29 18:48:07 -0400

    added custom login still needs to be wired up and also needs some cosmetic changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -27,5 +27,10 @@ module.exports = {
    
   
   },
-  plugins: [],
+  plugins: [
+    require('@tailwindcss/typography'),
+    require('@tailwindcss/forms'),
+    require('@tailwindcss/aspect-ratio'),
+    
+  ],
 }
