47632fccc3d61fbf73e82ce45887cf0021bba001
MailingDelgadoMedina <mailing.delgado.scitexas@gmail.com>
2023-05-02 01:12:15 -0400

    fixed login again

--- pages/_app.js
+++ pages/_app.js
@@ -12,28 +12,8 @@ const mina = Mina({ subsets: ['latin'], weight:['400', '700'] })
 
 export default function App({ Component, pageProps }) {
   return (
-<div className='bg-hsbg  min-h-full p-52    '> 
-  <Text
-              fontFamily="Minako"
-              fontSize="96px"
-              fontWeight="400"
-              color="rgba(254,148,42,1)"
-              lineHeight="158px"
-              textAlign="center"
-              display="block"
-              direction="column"
-              justifyContent="unset"
-              width="726px"
-              height="173px"
-              gap="unset"
-              alignItems="unset"
-              shrink="0"
-              position="relative"
-              padding="0px 0px 0px 50px"
-              marginLeft={380}
-              whiteSpace="pre-wrap"
-           
-            >Homeschool Book</Text>
+<div >
+ 
   
     <Authenticator
     
