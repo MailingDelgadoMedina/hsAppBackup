e699b08e99c9c5f62682013083742dbbbed648c8
MailingDelgadoMedina <mailing.delgado.scitexas@gmail.com>
2023-05-05 00:59:01 -0400

    close #1 Finished the side bar styles and adjusted footer. Added small navigation cards.

--- pages/myComponents/SideNav 2.js
+++ pages/myComponents/SideNav 2.js
@@ -79,7 +79,7 @@ export default function SideNav() {
               leaveFrom="opacity-100"
               leaveTo="opacity-0"
             >
-              <div className="fixed inset-0 bg-hsbg/80" />
+              <div className="fixed inset-0 bg-gray-900/80" />
             </Transition.Child>
 
             <div className="fixed inset-0 flex">
@@ -105,12 +105,12 @@ export default function SideNav() {
                     <div className="absolute left-full top-0 flex w-16 justify-center pt-5">
                       <button type="button" className="-m-2.5 p-2.5" onClick={() => setSidebarOpen(false)}>
                         <span className="sr-only">Close sidebar</span>
-                        <XMarkIcon className="h-6 w-6 text-hsgreen" aria-hidden="true" />
+                        <XMarkIcon className="h-6 w-6 text-white" aria-hidden="true" />
                       </button>
                     </div>
                   </Transition.Child>
                   {/* Sidebar component, swap this element with another sidebar if you like */}
-                  <div className="flex grow flex-col gap-y-5 overflow-y-auto bg-hsbg px-6 pb-4 ring-1 ring-hsorange/10">
+                  <div className="flex grow flex-col gap-y-5 overflow-y-auto bg-gray-900 px-6 pb-4 ring-1 ring-white/10">
                     <div className="flex h-16 shrink-0 items-center">
                       <a 
                       className="pointer"
@@ -120,7 +120,7 @@ export default function SideNav() {
                           height={32}
                         className="h-8 w-auto"
                         src="https://res.cloudinary.com/programandoconmei/image/upload/v1682278713/Hackthon/officiallogogreen_ljw6yp.png"
-                        alt="HS Logo"
+                        alt="Your Company"
                       />
                       </a>
                     
@@ -135,8 +135,8 @@ export default function SideNav() {
                                   href={item.href}
                                   className={classNames(
                                     item.current
-                                      ? 'bg-hsgreen text-hsbg'
-                                      : 'text-hsorange hover:text-hsgreen hover:bg-[#000642]',
+                                      ? 'bg-gray-800 text-white'
+                                      : 'text-gray-400 hover:text-white hover:bg-gray-800',
                                     'group flex gap-x-3 rounded-md p-2 text-sm leading-6 font-semibold'
                                   )}
                                 >
@@ -148,7 +148,7 @@ export default function SideNav() {
                           </ul>
                         </li>
                         <li>
-                          <div className="text-xs font-semibold leading-6 text-hsgreen">Necessities</div>
+                          <div className="text-xs font-semibold leading-6 text-gray-400">Necessities</div>
                           <ul role="list" className="-mx-2 mt-2 space-y-1">
                             {necessities.map((necessities) => (
                               <li key={necessities.name}>
@@ -156,12 +156,12 @@ export default function SideNav() {
                                   href={necessities.href}
                                   className={classNames(
                                     necessities.current
-                                      ? 'bg-hsgreen text-hsbg'
-                                      : 'text-hsorange hover:text-hsgreen hover:bg-[#000642]',
+                                      ? 'bg-gray-800 text-white'
+                                      : 'text-gray-400 hover:text-white hover:bg-gray-800',
                                     'group flex gap-x-3 rounded-md p-2 text-sm leading-6 font-semibold'
                                   )}
                                 >
-                                  <span className="flex h-6 w-6 shrink-0 items-center justify-center rounded-lg border border-hsorange  hover:border hover:border-hsgreen  bg-[#000642] text-[0.625rem] font-medium text-hsorange group-hover:text-hsgreen group-hover:border-hsgreen">
+                                  <span className="flex h-6 w-6 shrink-0 items-center justify-center rounded-lg border border-gray-700 bg-gray-800 text-[0.625rem] font-medium text-gray-400 group-hover:text-white">
                                     {necessities.initial}
                                   </span>
                                   <span className="truncate">{necessities.name}</span>
@@ -170,15 +170,15 @@ export default function SideNav() {
                             ))}
                           </ul>
                         </li>
-                        {/* <li className="mt-auto">
+                        <li className="mt-auto">
                           <a
                             href="#"
-                            className="group -mx-2 flex gap-x-3 rounded-md p-2 text-sm font-semibold leading-6 text-hsgreen hover:bg-[#000642] hover:text-hsorange"
+                            className="group -mx-2 flex gap-x-3 rounded-md p-2 text-sm font-semibold leading-6 text-gray-400 hover:bg-gray-800 hover:text-white"
                           >
                             <Cog6ToothIcon className="h-6 w-6 shrink-0" aria-hidden="true" />
                             Settings
                           </a>
-                        </li> */}
+                        </li>
                       </ul>
                     </nav>
                   </div>
@@ -191,7 +191,7 @@ export default function SideNav() {
         {/* Static sidebar for desktop */}
         <div className="hidden lg:fixed lg:inset-y-0 lg:z-50 lg:flex lg:w-72 lg:flex-col">
           {/* Sidebar component, swap this element with another sidebar if you like */}
-          <div className="flex grow flex-col gap-y-5 overflow-y-auto bg-hsbg border-4 border-hsgreen px-6 pb-4">
+          <div className="flex grow flex-col gap-y-5 overflow-y-auto bg-gray-900 px-6 pb-4">
             <div className="flex h-16 shrink-0 items-center">
             <a
               className="pointer"
@@ -220,8 +220,8 @@ export default function SideNav() {
                           href={item.href}
                           className={classNames(
                             item.current
-                              ? 'bg-hsgreen text-hsbg'
-                              : 'text-hsorange hover:text-hsgreen hover:bg-[#000642]',
+                              ? 'bg-gray-800 text-white'
+                              : 'text-gray-400 hover:text-white hover:bg-gray-800',
                             'group flex gap-x-3 rounded-md p-2 text-sm leading-6 font-semibold'
                           )}
                         >
@@ -233,7 +233,7 @@ export default function SideNav() {
                   </ul>
                 </li>
                 <li>
-                  <div className="text-xs font-semibold leading-6 text-hsgreen">Necessities</div>
+                  <div className="text-xs font-semibold leading-6 text-gray-400">Necessities</div>
                   <ul role="list" className="-mx-2 mt-2 space-y-1">
                     {necessities.map((necessities) => (
                       <li key={necessities.name}>
@@ -241,12 +241,12 @@ export default function SideNav() {
                           href={necessities.href}
                           className={classNames(
                             necessities.current
-                              ? 'bg-hsbg text-hsgreen'
-                              : 'text-hsorange hover:text-hsgreen hover:bg-[#000642]',
+                              ? 'bg-gray-800 text-white'
+                              : 'text-gray-400 hover:text-white hover:bg-gray-800',
                             'group flex gap-x-3 rounded-md p-2 text-sm leading-6 font-semibold'
                           )}
                         >
-                          <span className="flex h-6 w-6 shrink-0 items-center justify-center rounded-lg border border-hsorange bg-hsbg text-[0.625rem] font-medium text-hsorange group-hover:text-hsgreen group-hover:border-hsgreen">
+                          <span className="flex h-6 w-6 shrink-0 items-center justify-center rounded-lg border border-gray-700 bg-gray-800 text-[0.625rem] font-medium text-gray-400 group-hover:text-white">
                             {necessities.initial}
                           </span>
                           <span className="truncate">{necessities.name}</span>
@@ -255,7 +255,7 @@ export default function SideNav() {
                     ))}
                   </ul>
                 </li>
-                {/* <li className="mt-auto">
+                <li className="mt-auto">
                   <a
                     href="#"
                     className="group -mx-2 flex gap-x-3 rounded-md p-2 text-sm font-semibold leading-6 text-gray-400 hover:bg-gray-800 hover:text-white"
@@ -263,15 +263,15 @@ export default function SideNav() {
                     <Cog6ToothIcon className="h-6 w-6 shrink-0" aria-hidden="true" />
                     Settings
                   </a>
-                </li> */}
+                </li>
               </ul>
             </nav>
           </div>
         </div>
 
         <div className="lg:pl-72">
-          <div className="sticky top-0 z-40 flex h-16 shrink-0 items-center gap-x-4 border-b border-hsorange bg-[#000642] px-4 shadow-sm sm:gap-x-6 sm:px-6 lg:px-8">
-            <button type="button" className="-m-2.5 p-2.5 text-hsgreen hover:text-hsorange lg:hidden" onClick={() => setSidebarOpen(true)}>
+          <div className="sticky top-0 z-40 flex h-16 shrink-0 items-center gap-x-4 border-b border-gray-200 bg-white px-4 shadow-sm sm:gap-x-6 sm:px-6 lg:px-8">
+            <button type="button" className="-m-2.5 p-2.5 text-gray-700 lg:hidden" onClick={() => setSidebarOpen(true)}>
               <span className="sr-only">Open sidebar</span>
               <Bars3Icon className="h-6 w-6" aria-hidden="true" />
             </button>
@@ -285,19 +285,19 @@ export default function SideNav() {
                   Search
                 </label>
                 <MagnifyingGlassIcon
-                  className="pointer-events-none absolute inset-y-0 left-0 h-full w-5 text-hsorange/60"
+                  className="pointer-events-none absolute inset-y-0 left-0 h-full w-5 text-gray-400"
                   aria-hidden="true"
                 />
                 <input
                   id="search-field"
-                  className="block bg-hsbg h-full w-full border-0 py-0 pl-8 pr-0 text-hspink placeholder:text-hsorange focus:ring-0 sm:text-sm"
+                  className="block h-full w-full border-0 py-0 pl-8 pr-0 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm"
                   placeholder="Search..."
                   type="search"
                   name="search"
                 />
               </form>
               <div className="flex items-center gap-x-4 lg:gap-x-6">
-                <button type="button" className="-m-2.5 p-2.5 text-hsorange hover:text-hsgreen">
+                <button type="button" className="-m-2.5 p-2.5 text-gray-400 hover:text-gray-500">
                   <span className="sr-only">View notifications</span>
                   <BellIcon className="h-6 w-6" aria-hidden="true" />
                 </button>
@@ -310,17 +310,17 @@ export default function SideNav() {
                   <Menu.Button className="-m-1.5 flex items-center p-1.5">
                     <span className="sr-only">Open user menu</span>
                     <Image
-                      className="h-8 w-8 rounded-full bg-hsbg/50"
+                      className="h-8 w-8 rounded-full bg-gray-50"
                       width={32}
                       height={32}
                       src="https://res.cloudinary.com/programandoconmei/image/upload/v1682746951/Hackthon/profileIcon_g63gsv.png"
                       alt="profile pic"
                     />
                     <span className="hidden lg:flex lg:items-center">
-                      <span className="ml-4 text-sm text-hspink font-semibold leading-6 " aria-hidden="true">
+                      <span className="ml-4 text-sm font-semibold leading-6 text-gray-900" aria-hidden="true">
                         Tom Cook
                       </span>
-                      <ChevronDownIcon className="ml-2 h-5 w-5 text-hsgreen" aria-hidden="true" />
+                      <ChevronDownIcon className="ml-2 h-5 w-5 text-gray-400" aria-hidden="true" />
                     </span>
                   </Menu.Button>
                   <Transition
@@ -332,15 +332,15 @@ export default function SideNav() {
                     leaveFrom="transform opacity-100 scale-100"
                     leaveTo="transform opacity-0 scale-95"
                   >
-                    <Menu.Items className="absolute right-0 z-10 mt-2.5 w-32 origin-top-right rounded-md bg-hsbg py-2 shadow-lg ring-1 ring-gray-900/5 focus:outline-none">
+                    <Menu.Items className="absolute right-0 z-10 mt-2.5 w-32 origin-top-right rounded-md bg-white py-2 shadow-lg ring-1 ring-gray-900/5 focus:outline-none">
                       {userNavigation.map((item) => (
                         <Menu.Item key={item.name}>
                           {({ active }) => (
                             <a
                               href={item.href}
                               className={classNames(
-                                active ? 'bg-hsorange text-hsbg' : '',
-                                'block px-3 py-1 text-sm leading-6 hover:text-hsbg text-hsgreen'
+                                active ? 'bg-gray-50' : '',
+                                'block px-3 py-1 text-sm leading-6 text-gray-900'
                               )}
                             >
                               {item.name}
@@ -355,8 +355,8 @@ export default function SideNav() {
             </div>
           </div>
 
-          <main className="  pt-10  h-screen lg:h-fit xl:h-full">
-            <div className="px-4  sm:px-6 lg:px-8">
+          <main className="py-10">
+            <div className="px-4 sm:px-6 lg:px-8">
               {/* Your content */}
            <Dashboard/>
           
--- pages/myComponents/dashboard/Dashboard.js
+++ pages/myComponents/dashboard/Dashboard.js
@@ -24,10 +24,10 @@ function classNames(...classes) {
 
 function Dashboard() {
   return (
-    <div>
+    <div className='h-screen'>
 
 
-         <div className='bg-hsbg  '>
+         <div className='mb-10 mt-10 bg-hsbg  border-hsorange border-4 '>
 
   <Image
   className='bg-hsbg   ml-48 lg:ml-36 '
@@ -45,33 +45,33 @@ function Dashboard() {
 {/* Tags  */}
 
  <div>
-      <h2 className="text-sm font-medium text-gray-500">Pinned Projects</h2>
-      <ul role="list" className="mt-3 grid grid-cols-1 gap-5 sm:grid-cols-2 sm:gap-6 lg:grid-cols-4">
+      {/* <h2 className="text-sm font-medium text-gray-500">Pinned Projects</h2> */}
+      <ul role="list" className="mt-3 mb-24 grid grid-cols-1 gap-5 sm:grid-cols-2 sm:gap-6 lg:grid-cols-4  ">
         {projects.map((project) => (
-          <li key={project.name} className="col-span-1 flex rounded-md shadow-sm">
+          <li key={project.name} className="col-span-1  flex rounded-md shadow-sm">
             <div
               className={classNames(
                 project.bgColor,
-                'flex w-16 flex-shrink-0 items-center justify-center rounded-l-md text-sm font-medium text-white'
+                ' border-4 border-hsbg flex w-16 flex-shrink-0 items-center justify-center rounded-l-md text-sm font-medium text-hsbg hover:text-hsgreen'
               )}
             >
               {project.initials}
             </div>
-            <div className="flex flex-1 items-center justify-between truncate rounded-r-md border-b border-r border-t border-gray-200 bg-white">
+            <div className="flex flex-1 items-center justify-between truncate rounded-r-md border-4 hover:bg-hsbg hover:text-hsgreen border-hsbg bg-white">
               <div className="flex-1 truncate px-4 py-2 text-sm">
-                <a href={project.href} className="font-medium text-gray-900 hover:text-gray-600">
+                <a href={project.href} className="font-medium  text- hover:text-hsgreen">
                   {project.name}
                 </a>
-                <p className="text-gray-500">{project.members} Members</p>
+                {/* <p className="text-gray-500">{project.members} Members</p> */}
               </div>
               <div className="flex-shrink-0 pr-2">
-                <button
+                {/* <button
                   type="button"
                   className="inline-flex h-8 w-8 items-center justify-center rounded-full bg-transparent bg-white text-gray-400 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
                 >
                   <span className="sr-only">Open options</span>
                   <EllipsisVerticalIcon className="h-5 w-5" aria-hidden="true" />
-                </button>
+                </button> */}
               </div>
             </div>
           </li>
